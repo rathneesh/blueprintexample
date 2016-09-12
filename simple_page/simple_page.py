4c1c93d66ccebe4016f35b10b9435eae3d201316
rathneesh <rathneesh@gmail.com>
2016-09-12 11:59:25 +0530

    Update simple_page.py

--- simple_page/simple_page.py
+++ simple_page/simple_page.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, abort
 from jinja2 import TemplateNotFound
-
+#Added comment
 simple_page = Blueprint('simple_page', __name__,
                         template_folder='templates')
 
